20a09afa14a36e69374d0468417e97f683759187
Abdullah Al Mridul <abdullah.github@zohomail.com>
2025-03-29 19:38:46 +0600

    add a router for this server

--- backend/constants/config.ts
+++ backend/constants/config.ts
@@ -1,8 +1,11 @@
+// interfaces for types
 interface Config {
   PORT: number;
+  API_ENDPOINT: string;
 }
 
 //config constants for this server
 export const Config: Config = {
   PORT: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+  API_ENDPOINT: process.env.API_ENDPOINT ? process.env.API_ENDPOINT : "/api",
 };
--- backend/server.ts
+++ backend/server.ts
@@ -2,15 +2,19 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-// important imports
+// importing modules
 import express from "express";
 import { Config } from "./constants/config";
+import { authRouter } from "./routes/auth.routes";
 
 // initializing the server
 const server = express();
 
 // configure server to parse json body
 server.use(express.json());
+
+// adding routes to the server
+server.use(`${Config.API_ENDPOINT}/auth`, authRouter);
 
 // listening for request
 server.listen(Config.PORT, () => {
